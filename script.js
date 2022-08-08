2670353c8a4e4dc8be545a9015ea91c023a0dd37
heidegomes <heidegc@hotmail.com>
2022-08-08 19:32:18 -0300

    requisito 9 feito!

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-// // 4.Cria um quadro de pixels 5x5
-// let classe = 'pixel';
-// let grid = document.getElementById('pixel-board');
-
-// function criaSquare(classe) {
-//   let square = document.createElement('div');
-//   square.className = classe;
-//   return square;
-// }
-// function paiFilho(criafilho){
-//   grid.appendChild(criafilho);
-// }  
-// paiFilho(criaDiv('pixel'));
-  
-//   for (let index of classe) {
-//     let element = criaSquare(index);
-//     paiFilho(element);
-//   }
-
-
-// 7. A cor clicada é selecionada
-
 let elemPreviaSelected = document.querySelector('.selected');
 const startedPreviaSelected = document.querySelector('.selected');
 console.log(startedPreviaSelected);
@@ -50,38 +28,22 @@ const pixelBoard = document.querySelector('#pixel-board');
 pixelBoard.addEventListener('click', clicaNoPixel);
 
 // 9. retorna a cor do quadro quando clicar no botão limpar
+function clearBoard() {
+  const elements = pixelBoard.getElementsByClassName('pixel');
+  for (let i = 0; i < elements.length; i += 1) {
+    const element = elements[i];
+    element.className = 'pixel';
+  }
+}
 function createClearButton(buttonName) {
   const buttonContainer = document.querySelector('.buttons-container');
   const newButton = document.createElement('button');
   const newButtonID = 'clear-board';
   newButton.innerHTML = buttonName;
   newButton.id = newButtonID;
-  function clearBoard() {
-// quando clicar no botão as divs com class pixel tem que voltar pra classe inicial pixel
-  elemPreviaSelected.classList.remove.item(1);
-  }
+
   newButton.addEventListener('click', clearBoard);
   buttonContainer.appendChild(newButton);
 }
 
 createClearButton('Limpar');
-
-// function clearBoard() {
-//   const getButton = document.querySelector('#clear-board');
-//   console.log(getButton);
-//   const board = document.getElementsByClassName('pixel');
-//   // console.log(board);
-//   const filled = document.getElementsByClassName('color');
-//   // console.log(filled);
-//   const unfilled = document.getElementById('pixel');
-//   // console.log(unfilled);
-
-// }
-//   getButton.addEventListener('click', () => { // cria o escutador de eventos
-//     for (let index = 0; index < board.length; index += 1) { // passa por todos os elementos pixel
-//       if (board[index].classList === filled) { // verifica se o pixel possui a clas list color
-//         board[index].classList = unfilled; // subtitui pela classlist pixel
-//       }
-//     }
-//   });
-// clearBoard();
